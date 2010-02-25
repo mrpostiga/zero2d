1fb2a10fe8d8197b9d0cf1af455e26c78519de74
TheBuzzSaw <TheBuzzSaw@83a3be9c-2180-11df-b401-3b86f75e9ead>
2010-02-25 22:35:38 +0000

    hi

---
+++ trunk/source/Entity.cpp
@@ -0,0 +1,51 @@
+/**
+ *  This file is part of Zero2D.
+ *
+ *  Zero2D is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Zero2D is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Zero2D.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Entity.h"
+
+list<Entity*> Entity::_entities;
+
+Entity::Entity(Sprite* inSprite) : _sprite(inSprite), _pulseCount(0)
+{
+    _entities.push_back(this);
+}
+
+Entity::~Entity()
+{
+    _entities.remove(this);
+}
+
+void Entity::unloadAll()
+{
+    while (!_entities.empty()) delete _entities.back();
+}
+
+void Entity::pulse()
+{
+    ++_pulseCount;
+
+    if (_pulseCount >= _sprite->getDuration(_args.index))
+    {
+        _args.index = (_args.index + 1) % _sprite->getNumFrames();
+        _pulseCount = 0;
+    }
+}
+
+void Entity::flip()
+{
+    _args.facingRight = !_args.facingRight;
+}
---
+++ trunk/source/Entity.h
@@ -0,0 +1,51 @@
+/**
+ *  This file is part of Zero2D.
+ *
+ *  Zero2D is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Zero2D is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Zero2D.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _ENTITY_H_
+#define _ENTITY_H_
+
+#include "Sprite.h"
+
+#include <list>
+using namespace std;
+
+class Entity
+{
+    public:
+        Entity(Sprite* inSprite);
+        virtual ~Entity();
+
+        static void unloadAll();
+
+        void pulse();
+        void flip();
+        void draw();
+
+    private:
+        static list<Entity*> _entities;
+
+        Sprite* _sprite;
+        DrawArgs _args;
+        int _pulseCount;
+};
+
+inline void Entity::draw()
+{
+    _sprite->draw(_args);
+}
+
+#endif
