772606edd1c3552c5ad5880edb9af75b5cd792a7
Cdaragorn1 <Cdaragorn1@83a3be9c-2180-11df-b401-3b86f75e9ead>
2010-07-09 23:01:48 +0000

    Added fading support to the fragment shader!

--- trunk/data/shaders/high/sprite.fs
+++ trunk/data/shaders/high/sprite.fs
@@ -1,13 +1,17 @@
 #version 130
 
+uniform sampler2D Texture;
+uniform float fade;
+
 in vec2 tex;
 out vec4 gl_FragColor;
-
-uniform sampler2D Texture;
 
 void main()
 {
     //vec3 temp = vec3(texture(Texture, tex));
     //gl_FragColor = vec4(temp, 1.0);
-    gl_FragColor = texture(Texture, tex);
+    vec4 color = texture(Texture, tex);
+    color.a *= fade;
+    color.a = clamp(color.a, 0.0, 1.0);
+    gl_FragColor = color;
 }
