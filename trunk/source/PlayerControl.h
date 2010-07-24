d746ff808158930604570f2d55ee64031184ff7b
Cdaragorn1 <Cdaragorn1@83a3be9c-2180-11df-b401-3b86f75e9ead>
2010-07-24 02:58:12 +0000

    oops

---
+++ trunk/source/PlayerControl.h
@@ -0,0 +1,22 @@
+#ifndef PLAYERCONTROL_H
+#define PLAYERCONTROL_H
+
+#include "Control.h"
+
+#include "Fighter.h"
+
+class PlayerControl : public Control
+{
+    public:
+        PlayerControl(Entity* inEntity);
+        virtual ~PlayerControl();
+
+        virtual void update();
+
+        void onEvent(State::Event inEvent);
+
+    protected:
+    private:
+};
+
+#endif // PLAYERCONTROL_H
