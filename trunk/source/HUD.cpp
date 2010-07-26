237a0a3e663d46a33f05948f2e35a3e86c8031c5
TheBuzzSaw <TheBuzzSaw@83a3be9c-2180-11df-b401-3b86f75e9ead>
2010-07-26 20:43:23 +0000

    refactoring our lovely HUD

---
+++ trunk/source/HUD.cpp
@@ -0,0 +1,54 @@
+/**
+ *  This file is part of Zero2D.
+ *
+ *  Zero2D is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Zero2D is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Zero2D.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "HUD.h"
+#include "DisplayEngine.h"
+
+#include <iostream>
+using namespace std;
+
+HUD::HUD(SpriteProgram& inProgram)
+{
+    mProgram = &inProgram;
+    mProjection.orthographic(1.0f, DisplayEngine::getAspectRatio());
+}
+
+HUD::~HUD()
+{
+}
+
+void HUD::add(Widget* inWidget)
+{
+    mWidgets.push_back(inWidget);
+}
+
+void HUD::remove(Widget* inWidget)
+{
+    mWidgets.remove(inWidget);
+}
+
+void HUD::display()
+{
+    for (list<Widget*>::iterator i = mWidgets.begin();
+        i != mWidgets.end(); ++i)
+    {
+        Widget* w = *i;
+        Matrix3D MVPM(mProjection);
+        MVPM.multiply(w->matrix());
+        w->display();
+    }
+}
---
+++ trunk/source/Widget.h
@@ -0,0 +1,77 @@
+/**
+ *  This file is part of Zero2D.
+ *
+ *  Zero2D is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Zero2D is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Zero2D.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include "Vector2D.h"
+#include "Matrix3D.h"
+
+class Widget
+{
+    public:
+        enum MouseState { OUT = 0, HOVER = 1, PRESS = 2 };
+
+        class EventListener
+        {
+            public:
+                virtual void onWidgetMouseHover() {}
+                virtual void onWidgetMouseClick() {}
+        };
+
+        Widget();
+        virtual ~Widget();
+
+        virtual void display() = 0;
+        virtual void onMouseStateChange();
+
+        void changeMouseState(MouseState inState);
+        void registerEventListener(EventListener* inListener);
+
+        inline bool isEnabled() { return mEnabled; }
+        inline bool isDisabled() { return !mEnabled; }
+        inline bool isVisible() { return mVisible; }
+        inline bool canFocus() { return mCanFocus; }
+
+        inline void enable() { mEnabled = true; }
+        inline void enable(bool inEnable) { mEnabled = inEnable; }
+        inline void disable() { mEnabled = false; }
+        inline void setVisible(bool inVisible) { mVisible = inVisible; }
+
+        inline const Matrix3D& matrix() const { return mMatrix; }
+
+    protected:
+        MouseState mCurrentState;
+        MouseState mLastState;
+
+        Matrix3D mMatrix;
+        Point mLocation;
+        Point mSize;
+        Point mObjectUL;
+        Point mObjectLR;
+        Pixel mPixelUL;
+        Pixel mPixelLR;
+
+    private:
+        bool mEnabled;
+        bool mVisible;
+        bool mCanFocus;
+
+        EventListener* mListener;
+};
+
+#endif
